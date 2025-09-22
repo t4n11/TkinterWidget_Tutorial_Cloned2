a84932550995b11335aca21942e85ae466e5d4cf
ike.lionel <ike.lionel@LAPTOP14129>
2025-09-22 11:42:03 +0100

    Add project files.

---
+++ TkinterWidget_Tutorial.py
@@ -0,0 +1,4 @@
+# Follow the tasks given in the PDF's for each Widget, these tasks are on Teams.
+"""Complete EACH widget in its own python file (.py) and the right click on the 
+.py file you want to run and select -> Set as Startup File"""
+
